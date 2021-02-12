dcef40fd4f3f381cdc40f275d5e27779a3c8d3ed
Lucas <lucaszawadneak@gmail.com>
2021-02-12 11:29:59 -0300

    Header | Limit page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LimitComponent } from './components/limit/limit.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LimitComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/limit/limit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-limit',
+  templateUrl: './limit.component.html',
+  styleUrls: ['./limit.component.css']
+})
+export class LimitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
