53972352dd353e38338cc860656fd4d5fc81f23f
Lucas <lucaszawadneak@gmail.com>
2021-02-17 09:36:34 -0300

    Counter with debounce time operator

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 import { StringCounterService } from 'src/app/services/string-counter.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css'],
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, OnDestroy {
   constructor(private StringConter: StringCounterService) {}
 
   textData: string = '';
@@ -15,22 +17,30 @@ export class CounterComponent implements OnInit {
   count: number = 0;
   limitDiff: number = 0;
 
+  searchEntry: Subject<string> = new Subject<string>();
+
   ngOnInit(): void {
     this.limit = this.StringConter.getLimit();
     if (this.limit !== 0) {
       this.limitDiff = this.StringConter.handleLimitDif(0);
     }
-  }
 
-  handleChange(event: any): void {
-    //AO APERTAR ESPAÇO, BACKSPACE E LETRA V
-    if (event.keyCode == 32 || event.keyCode == 8 || event.keyCode == 86) {
-      this.StringConter.setText(event.target.value);
+    //APENAS EXECUTA AS AÇÕES APÓS 300ms DO ULTIMO EVENTO
+    this.searchEntry.pipe(debounceTime(300)).subscribe((data) => {
+      this.StringConter.setText(data);
 
       this.count = this.StringConter.countWords();
       if (this.limit > 0) {
         this.limitDiff = this.StringConter.handleLimitDif(this.count);
       }
-    }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.searchEntry.unsubscribe();
+  }
+
+  handleChange(event: any): void {
+    this.searchEntry.next(event.target.value);
   }
 }
